70881c8a9258bee4e80bcc89ba1b46b9e6284fd5
Bang Xiao <bangxiao0927@gmail.com>
2026-07-02 09:20:59 -0700

    移除 CnUsername 空目录残留

--- 2b2t/run.bat
+++ 2b2t/run.bat
@@ -2,7 +2,7 @@
 title 2b2t
 :loop
 echo Starting 2b2t server...
-%cd%.\jdk-21\bin\java.exe -Xms8G -Xmx8G -XX:SoftMaxHeapSize=6G -XX:+UnlockExperimentalVMOptions -Dfile.encoding=UTF-8 -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+UseZGC -XX:+ZGenerational -XX:-ZProactive -XX:ZUncommitDelay=5 --add-modules jdk.incubator.vector -jar leaf-1.21.8-60.jar --nogui
+%cd%.\jdk-21\bin\java.exe -Xms8G -Xmx8G -XX:SoftMaxHeapSize=6G -XX:+UnlockExperimentalVMOptions -Dfile.encoding=UTF-8 -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+UseZGC -XX:+ZGenerational -XX:-ZProactive -XX:ZUncommitDelay=5 --add-modules jdk.incubator.vector -jar leaf-26.2-14.jar --nogui
 echo Server stopped, restarting in 60s...
 timeout /t 60 /nobreak >nul
 goto loop
