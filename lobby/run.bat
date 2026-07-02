24167d41239ab0c6ee919e8a08db67059e573f4d
Bang Xiao <bangxiao0927@gmail.com>
2026-07-01 23:02:35 -0700

    fix(lobby): correct run.bat JAR from purpur-1.21.3 to paper.jar

--- lobby/run.bat
+++ lobby/run.bat
@@ -2,7 +2,7 @@
 title lobby-tech
 :loop
 echo 启动大厅服务器...
-java -Xms2G -Xmx2G -XX:+IgnoreUnrecognizedVMOptions -XX:+UnlockExperimentalVMOptions -Dfile.encoding=UTF-8 -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:-UseCompressedClassPointers -XX:MaxDirectMemorySize=4G -XX:-UseG1GC -XX:+UseZGC -XX:+ZGenerational -XX:-ZProactive -XX:ZCollectionIntervalMinor=0.95 -XX:ZUncommitDelay=4 --add-modules jdk.incubator.vector -jar purpur-1.21.3-2358.jar --nogui
+java -Xms2G -Xmx2G -XX:+IgnoreUnrecognizedVMOptions -XX:+UnlockExperimentalVMOptions -Dfile.encoding=UTF-8 -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:-UseCompressedClassPointers -XX:MaxDirectMemorySize=4G -XX:-UseG1GC -XX:+UseZGC -XX:+ZGenerational -XX:-ZProactive -XX:ZCollectionIntervalMinor=0.95 -XX:ZUncommitDelay=4 --add-modules jdk.incubator.vector -jar paper.jar --nogui
 echo lobby关闭，1分钟后自动重启...
 timeout /t 60 /nobreak >nul
 goto loop
